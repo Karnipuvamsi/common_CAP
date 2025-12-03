2fa32cebbdf97a742ff1d0213f70d6fe619eab9a
Karnipu Vamsi <karnipu.vamsi@genpact.com>
2025-12-03 14:47:44 +0000

    capm

--- srv/server.js
+++ srv/server.js
@@ -1,11 +1,18 @@
 const cds = require('@sap/cds');
 const cov2ap = require('@cap-js-community/odata-v2-adapter');
+const express = require("express");
+const path = require("path");
 
 cds.on('bootstrap', app => {
   app.use(cov2ap({
     path: '/odata/v2',
     targetPath: '/odata/v4'
   }));
+
+  app.use(
+    "/resources/ui5Lib",
+    express.static(path.join(__dirname, "static/ui5Lib"))
+  );
 });
 
 module.exports = cds.server;
